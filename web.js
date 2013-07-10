7c8e82a7d004560c57fe3277790def1ac53d31ed
Andrew Chan <sleepykid@gmail.com>
2013-07-10 13:50:01 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexFileBuffer= fs.readFileSync("index.html");
+  var greeting=indexFileBuffer.toString;
+  response.send(greeting);
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
