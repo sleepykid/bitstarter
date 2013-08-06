411f4ca1b18370b8195880f0bbfc83db6845f8a6
Andrew Chan <sleepykid@gmail.com>
2013-08-06 00:26:00 +0000

    edited product title in index.html

--- web.js
+++ web.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var fs=require('fs');
+var htmlfile="index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var indexFileBuffer= fs.readFileSync("index.html");
-  var greeting=indexFileBuffer.toString();
-  response.send(greeting);
+  //var indexFileBuffer= fs.readFileSync("index.html");
+  //var greeting=indexFileBuffer.toString();
+  //response.send(greeting);
   //response.send('Hello World 2!');
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
